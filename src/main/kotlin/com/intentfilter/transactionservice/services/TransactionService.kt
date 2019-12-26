880f102c5b4d5d2fbad3edfc0c03054ce8e0501a
nishkarsh <nishkarsh4@gmail.com>
2019-12-26 10:08:10 +0530

    Handle scenario when invalid account IDs supplied for transaction.

--- src/main/kotlin/com/intentfilter/transactionservice/services/TransactionService.kt
+++ src/main/kotlin/com/intentfilter/transactionservice/services/TransactionService.kt
@@ -3,6 +3,7 @@ package com.intentfilter.transactionservice.services
 import com.intentfilter.transactionservice.models.Money
 import com.intentfilter.transactionservice.models.Transaction
 import com.intentfilter.transactionservice.repositories.TransactionRepository
+import javassist.NotFoundException
 import javax.inject.Inject
 
 open class TransactionService @Inject constructor(
@@ -10,11 +11,14 @@ open class TransactionService @Inject constructor(
 ) {
     open fun create(transaction: Transaction): Transaction {
         val remitterAccount = accountService.getAccountById(transaction.remitterAccount.id)
+            ?: throw NotFoundException("Account with ID ${transaction.remitterAccount.id} does not exist")
+
         val beneficiaryAccount = accountService.getAccountById(transaction.beneficiaryAccount.id)
+            ?: throw NotFoundException("Account with ID ${transaction.beneficiaryAccount.id} does not exist")
 
         // TODO Start transaction with both Accounts Locked
-        accountService.debit(remitterAccount!!, Money(transaction.currencyCode, transaction.amount))
-        accountService.credit(beneficiaryAccount!!, Money(transaction.currencyCode, transaction.amount))
+        accountService.debit(remitterAccount, Money(transaction.currencyCode, transaction.amount))
+        accountService.credit(beneficiaryAccount, Money(transaction.currencyCode, transaction.amount))
         val createdTransaction = repository.create(transaction)
         // TODO Commit transaction and release lock
 
--- src/test/kotlin/com/intentfilter/transactionservice/services/TransactionServiceTest.kt
+++ src/test/kotlin/com/intentfilter/transactionservice/services/TransactionServiceTest.kt
@@ -4,17 +4,18 @@ import com.intentfilter.transactionservice.models.Account
 import com.intentfilter.transactionservice.models.Money
 import com.intentfilter.transactionservice.models.Transaction
 import com.intentfilter.transactionservice.repositories.TransactionRepository
+import com.nhaarman.mockitokotlin2.*
 import io.github.glytching.junit.extension.random.Random
 import io.github.glytching.junit.extension.random.RandomBeansExtension
+import javassist.NotFoundException
 import org.hamcrest.core.Is.`is`
 import org.hamcrest.junit.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.extension.Extensions
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito.`when`
-import org.mockito.Mockito.inOrder
 import org.mockito.junit.jupiter.MockitoExtension
 import java.util.*
 
@@ -32,9 +33,9 @@ internal class TransactionServiceTest {
         @Random transaction: Transaction, @Random remitter: Account, @Random beneficiary: Account, @Random transactionId: UUID
     ) {
         val createdTransaction = transaction.copy(id = transactionId)
-        `when`(accountService.getAccountById(transaction.remitterAccount.id)).thenReturn(remitter)
-        `when`(accountService.getAccountById(transaction.beneficiaryAccount.id)).thenReturn(beneficiary)
-        `when`(repository.create(transaction)).thenReturn(createdTransaction)
+        whenever(accountService.getAccountById(transaction.remitterAccount.id)).thenReturn(remitter)
+        whenever(accountService.getAccountById(transaction.beneficiaryAccount.id)).thenReturn(beneficiary)
+        whenever(repository.create(transaction)).thenReturn(createdTransaction)
 
         val returnedTransaction = transactionService.create(transaction)
 
@@ -42,5 +43,26 @@ internal class TransactionServiceTest {
         inOrder.verify(accountService).debit(remitter, Money(transaction.currencyCode, transaction.amount))
         inOrder.verify(accountService).credit(beneficiary, Money(transaction.currencyCode, transaction.amount))
         assertThat(returnedTransaction, `is`(createdTransaction))
+    }
+
+    @Test
+    internal fun shouldThrowErrorWhenRemitterAccountNotFound(@Random transaction: Transaction, @Random beneficiary: Account) {
+        whenever(accountService.getAccountById(transaction.remitterAccount.id)).thenReturn(null)
+
+        assertThrows<NotFoundException> { transactionService.create(transaction) }
+
+        verify(accountService, never()).debit(any(), any())
+        verify(accountService, never()).credit(any(), any())
+    }
+
+    @Test
+    internal fun shouldThrowErrorWhenBeneficiaryAccountNotFound(@Random transaction: Transaction, @Random remitter: Account) {
+        whenever(accountService.getAccountById(transaction.remitterAccount.id)).thenReturn(remitter)
+        whenever(accountService.getAccountById(transaction.beneficiaryAccount.id)).thenReturn(null)
+
+        assertThrows<NotFoundException> { transactionService.create(transaction) }
+
+        verify(accountService, never()).debit(any(), any())
+        verify(accountService, never()).credit(any(), any())
     }
 }

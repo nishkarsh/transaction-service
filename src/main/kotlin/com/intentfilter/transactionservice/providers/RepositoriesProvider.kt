f6f1091ded14b8919e2c21a510b18830a911563c
nishkarsh <nishkarsh4@gmail.com>
2019-12-26 10:09:52 +0530

    Add missing contract for AccountRepository.

--- src/main/kotlin/com/intentfilter/transactionservice/providers/RepositoriesProvider.kt
+++ src/main/kotlin/com/intentfilter/transactionservice/providers/RepositoriesProvider.kt
@@ -1,10 +1,12 @@
 package com.intentfilter.transactionservice.providers
 
+import com.intentfilter.transactionservice.repositories.AccountRepository
 import com.intentfilter.transactionservice.repositories.TransactionRepository
 import org.glassfish.hk2.utilities.binding.AbstractBinder
 
 object RepositoriesProvider : AbstractBinder() {
     override fun configure() {
         bindAsContract(TransactionRepository::class.java)
+        bindAsContract(AccountRepository::class.java)
     }
 }
